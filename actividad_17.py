ff4d21f2ea3b905742aa140df11d7f9ddd2522ca
Y.V <straysheep753@gmail.com>
2025-08-10 12:06:58 -0600

    2. implementación de métodos

--- actividad_17.py
+++ actividad_17.py
@@ -1,5 +1,3 @@
-main_kart = []
-fav_list = []
 class Carrito:
     def __init__(self, user, cart):
         self.user = user
@@ -7,13 +5,18 @@ class Carrito:
     #append()
     def add_to_cart(self, product):
         self.cart.append(product)
-        print(f"{product} agregado exitosamente: {self.cart}")
+        print(f"{product} agregado exitosamente")
     #remove
     def remove_from_cart(self):
-        pass
+        doomed_input = input("Coloque el producto que quiere eliminar")
+        for item in self.cart:
+            if doomed_input.lower() == item.lower():
+                print(f"{item} a sido eliminado exitosamente")
+                self.cart.remove(item)
     #extend()
-    def import_from_liked(self):
-        pass
+    def import_from_liked(self, followed_import):
+        self.cart.extend(followed_import)
+        print(f"Ahora el carrito contiene: {self.cart}")
 
 class Favoritos:
     def __init__(self, user, followed):
@@ -21,10 +24,58 @@ class Favoritos:
         self.followed = followed
     #sort
     def positive_sort(self):
-        pass
+        print(f"Su lista de favoritos a sido ordenada de manera ascendente:{self.followed.sort()}")
     #reverse()
     def negative_sort(self):
-        pass
+        print(f"Su lista de favoritos a sido ordenada de manera descendente:{self.followed.reverse()}")
     #clear()
     def doom_nuke(self):
-        pass
+        print("Su lista de favoritos será purgada...")
+        self.followed.clear()
+
+temp_kart = []
+fav_list = []
+#Objetos:
+user_cart = Carrito("Usuario_1", [])
+user_followed = Favoritos("Usuario_1", [])
+# Programa
+key = True
+while key:
+    try:
+        print("----------Menú-----------")
+        ops = input("1.Agregar al carrito\n"
+                    "2.Agregar a favoritos\n"
+                    "3.Eliminar del carrito\n"
+                    "4.Ordenar favoritos.\n"
+                    "Opción: ")
+        match ops:
+            case "1":
+                max_count = int(input("Cuántos productos desea agregar al carrito: "))
+                for temp_product in range(max_count):
+                    product_x = input("Coloque el producto: ")
+                    user_cart.add_to_cart(product_x)
+                print(user_cart.cart)
+            case "2":
+                max_count = int(input("Cuántos productos desea agregar a favoritos: "))
+                for temp_product in range(max_count):
+                    followed_x = input("Coloque el producto: ")
+                    user_followed.followed.append(followed_x)
+                print(user_followed.followed)
+                #Merge lists
+                merge_ops = input("Desea agregar esto al carrito Y/N: ")
+                if merge_ops.lower() == "y":
+                    user_cart.import_from_liked(user_followed.followed)
+                elif merge_ops.lower() == "n":
+                    print("Regresando al programa")
+                else:
+                    print("Eso no es un valor aceptado, regresando a menú...")
+
+            case "3":
+                pass
+            case "4":
+                key = False
+                print("Gracias por usar el programa")
+    except ValueError:
+        print("Eso no es un número, pe")
+    except Exception as e:
+        print(f"Error inesperado: {e}")
